db3ee005f90fa2cb6207ceccd648350bab9dc876
haley-at-acta <haley@actasolutions.io>
2023-11-20 16:05:23 -0500

    Added failing test

--- index.spec.ts
+++ index.spec.ts
@@ -5,4 +5,8 @@ describe('factorial', () => {
   it('should call 5', () => {
     expect(factorial(5)).to.deep.equal(120);
   });
+
+  it('should call 10', () => {
+    expect(factorial(10)).to.deep.equal(3628801);
+  });
 });
